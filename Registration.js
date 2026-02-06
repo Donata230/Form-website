7a6e434f920e0997b16f9f47dc9f1d1614974b7a
Donata230 <donatageorgewill@gmail.com>
2026-02-06 14:33:30 +0100

    update

--- Registration.js
+++ Registration.js
@@ -1,15 +1,37 @@
-function signup() {
-  let user = document.getElementById("newUser").value;
-  let pass = document.getElementById("newEmail").value;
-  let pass = document.getElementById("newCountry").value;
-  let pass = document.getElementById("newNumber").value;
-  let pass = document.getElementById("newPass").value;
+const regBtn = document.querySelector ("#reg-btn");
+const regForm = document.querySelector(".Register-card");
+regBtn.addEventListener("click",function(e) {
+  e.preventDefault()
+  const user = document.getElementById("newUser").value;
+  const email = document.getElementById("newEmail").value;
+  const country = document.getElementById("newCountry").value;
+  const number = document.getElementById("newNumber").value;
+  const password = document.getElementById("newPass").value;
+  
+  //Checking user fields before successful registration
+  if (!user || email=== "" || !country || !number || !password) {
+    alert("All fields required")
+    return;
+  }
+  
+  const userData = {
+    fullName: user,
+    userEmail: email,
+    userCountry: country,
+    userNumber: number,
+    userPassword: password,
+  }
+  
+  alert("Account created successfully 🎉")
+  window.location.href = "index.html"
+  console.log(userData)
+  regForm.reset()
 
-  localStorage.setItem("username", user);
+  /*localStorage.setItem("username", user);
   localStorage.setItem("email", user);
   localStorage.setItem("country", user);
   localStorage.setItem("number", user);
-  localStorage.setItem("password", pass);
+  localStorage.setItem("password", pass);*/
 
-  alert("Account created successfully 🎉");
-}
+  /*);*/
+})
